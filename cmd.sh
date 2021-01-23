943c0eeb1561818d1414d804563d612b5742cf55
c00cjz00 <summerhill001@gmail.com>
2021-01-23 09:30:36 +0800

    init

--- cmd.sh
+++ cmd.sh
@@ -2,5 +2,5 @@
 python demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
   --input cat.png \
   --output cat_output.png \
-  --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl
+  --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl MODEL.DEVICE cpu
 
--- serve_model.py
+++ serve_model.py
@@ -26,7 +26,7 @@ def predict(filename):
     mycmd='python demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
             --input static/' + filename +' \
             --output static/output_' + filename + ' \
-            --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl'    
+            --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl MODEL.DEVICE cpu'    
     cmd(mycmd)
     return mycmd 
 
