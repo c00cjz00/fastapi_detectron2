d5230cb6bba1e32b8e88ab65f39393abfcec9621
c00cjz00 <summerhill001@gmail.com>
2021-01-23 09:53:46 +0800

    init

--- serve_model.py
+++ serve_model.py
@@ -24,8 +24,8 @@ def read_imagefile(file) -> Image.Image:
 # 函式: 預測圖片
 def predict(filename):
     mycmd='python demo.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
-            --input static/' + filename +' \
-            --output static/output_' + filename + ' \
+            --input "static/' + filename +'" \
+            --output "static/output_' + filename + '" \
             --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl MODEL.DEVICE cpu'    
     cmd(mycmd)
     return mycmd 
